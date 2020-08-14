fb724bef2304fde73368de158cc15c8e265021ea
Alex K <>
2020-08-14 18:38:43 +0300

    Quiz implementation based on which user gets suggested activities to try!

--- src/main/java/com/webapp/groupproject/controllers/HomeController.java
+++ src/main/java/com/webapp/groupproject/controllers/HomeController.java
@@ -5,8 +5,22 @@
  */
 package com.webapp.groupproject.controllers;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.webapp.groupproject.pojo.Activity;
+import com.webapp.groupproject.utils.QuizFilters;
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  *
@@ -14,9 +28,37 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class HomeController {
-    
+
     @RequestMapping("/")
-    public String home(){
+    public String home() {
         return "newjsf";
+    }
+
+    @GetMapping("quiz")
+    public String showQuiz() {
+        return "quiz";
+    }
+
+    @GetMapping("quiz/answers")
+    @ResponseBody
+    public String showQuizAnswers(
+            @RequestParam String answer1,
+            @RequestParam String answer2,
+            @RequestParam String answer3,
+            @RequestParam String answer4
+    ) throws MalformedURLException, IOException {
+        ObjectMapper om = new ObjectMapper();
+        List<Activity> activities = om.readValue(new File(
+                "src/main/resources/activities.json"
+        ), new TypeReference<List<Activity>>() {
+        });
+        List<Activity> filteredActivities = QuizFilters.filterActivitiesBasedOnAnswers(answer1,
+                answer2, answer3, answer4, activities);
+        if (filteredActivities.isEmpty()) {
+            return null;
+        } else {
+            return om.writeValueAsString(filteredActivities);
+        }
+
     }
 }
